f89f28aeda7cfd044ef332511359df1d0a3fcf10
Danny <57102573+F4KER-X@users.noreply.github.com>
2025-01-19 09:37:27 -0500

    Setup firebase config with expo (#23)
    * Setup firebase config with expo
    
    * fixed formatting
    
    ---------
    
    Co-authored-by: Danny <dannnymousa1999@hotmail.com>

---
+++ Polaris-FE/services/firestoreService.ts
@@ -0,0 +1,16 @@
+import { db } from '@/firebase/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
+
+export const fetchDepartmentsAndServices = async () => {
+  try {
+    const data = await getDocs(collection(db, 'CampusDepartmentAndServices'));
+
+    return data.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    //should have better error handeling in the future, for testing purpose, will keep it like this
+    console.log(error);
+  }
+};
