f59799174e7337ce1edeae74571b5d4c154b529e
Danny <57102573+F4KER-X@users.noreply.github.com>
2025-02-07 15:06:09 -0500

    testing (#77)
    * testing
    
    * testing
    
    * testing

--- Polaris-FE/firebase/firebaseConfig.ts
+++ Polaris-FE/firebase/firebaseConfig.ts
@@ -4,7 +4,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 
-export const config = {
+export const configF = {
   apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
   authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
   projectId: Constants.expoConfig?.extra?.firebaseProjectId,
@@ -15,7 +15,7 @@ export const config = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(config) : getApps()[0];
+const app = !getApps().length ? initializeApp(configF) : getApps()[0];
 
 // Firestore
 export const db = getFirestore(app);
