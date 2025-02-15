a990e5727d51fafc4e19c6db3d181f4598dfeb32
RenalWasouf <123764157+RenalWasouf@users.noreply.github.com>
2025-02-14 19:37:40 -0500

    Update useMapLocation.ts

--- Polaris-FE/hooks/useMapLocation.ts
+++ Polaris-FE/hooks/useMapLocation.ts
@@ -19,7 +19,6 @@ export const useMapLocation = () => {
 
   useEffect(() => {
     let subscriber: Location.LocationSubscription | null = null;
-    let intervalId: NodeJS.Timeout | null = null;
 
     (async () => {
       try {
@@ -48,22 +47,6 @@ export const useMapLocation = () => {
             });
           }
         );
-
-        // Periodically fetch location every second
-        intervalId = setInterval(async () => {
-          const newLocation = await Location.getCurrentPositionAsync({
-            accuracy: Location.Accuracy.High,
-          });
-          const { latitude, longitude } = newLocation.coords;
-          setLocation({ latitude, longitude });
-
-          setRegion({
-            latitude,
-            longitude,
-            latitudeDelta: 0.02,
-            longitudeDelta: 0.02,
-          });
-        }, 1000);
       } catch (error) {
         setPermissionStatus('error');
       }
@@ -72,9 +55,6 @@ export const useMapLocation = () => {
     return () => {
       if (subscriber) {
         subscriber.remove();
-      }
-      if (intervalId) {
-        clearInterval(intervalId);
       }
     };
   }, []);
